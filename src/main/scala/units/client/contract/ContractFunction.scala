71443b49ec7977b6ae74f4a51b80ac8ee5500a7e
Sergey Nazarov <phearnot@renee.ru>
2024-09-02 15:13:51 +0300

    wip

--- src/main/scala/units/client/contract/ContractFunction.scala
+++ src/main/scala/units/client/contract/ContractFunction.scala
@@ -11,8 +11,8 @@ import cats.syntax.either.*
 
 abstract class ContractFunction(name: String, reference: BlockHash, extraArgs: Either[CommonError, List[EVALUATED]]) {
   def toFunctionCall(blockHash: BlockHash, transfersRootHash: Digest, lastClToElTransferIndex: Long): Job[FUNCTION_CALL] = (for {
-    hash <- CONST_STRING(blockHash)
-    ref  <- CONST_STRING(reference)
+    hash <- CONST_STRING(toHexNoPrefix(blockHash.getBytes))
+    ref  <- CONST_STRING(toHexNoPrefix(reference.getBytes))
     trh  <- CONST_STRING(toHexNoPrefix(transfersRootHash))
     xtra <- extraArgs
   } yield FUNCTION_CALL(
