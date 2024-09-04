15eb8412ab610352dec4c53d0fa4578efa48ebe0
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-09-04 11:33:01 +0400

    Fix

--- src/main/scala/units/client/contract/ContractFunction.scala
+++ src/main/scala/units/client/contract/ContractFunction.scala
@@ -1,18 +1,19 @@
 package units.client.contract
 
+import cats.syntax.either.*
 import com.wavesplatform.common.merkle.Digest
 import com.wavesplatform.common.state.ByteStr
 import com.wavesplatform.lang.CommonError
 import com.wavesplatform.lang.v1.FunctionHeader
 import com.wavesplatform.lang.v1.compiler.Terms.{CONST_BYTESTR, CONST_LONG, CONST_STRING, EVALUATED, FUNCTION_CALL}
-import units.{BlockHash, ClientError, Job}
+import com.wavesplatform.utils.EthEncoding.cleanHexPrefix
 import units.util.HexBytesConverter.toHexNoPrefix
-import cats.syntax.either.*
+import units.{BlockHash, ClientError, Job}
 
 abstract class ContractFunction(name: String, reference: BlockHash, extraArgs: Either[CommonError, List[EVALUATED]]) {
   def toFunctionCall(blockHash: BlockHash, transfersRootHash: Digest, lastClToElTransferIndex: Long): Job[FUNCTION_CALL] = (for {
-    hash <- CONST_STRING(blockHash.drop(2))
-    ref  <- CONST_STRING(reference.drop(2))
+    hash <- CONST_STRING(cleanHexPrefix(blockHash))
+    ref  <- CONST_STRING(cleanHexPrefix(reference))
     trh  <- CONST_STRING(toHexNoPrefix(transfersRootHash))
     xtra <- extraArgs
   } yield FUNCTION_CALL(
