84fc4fdff18271b34610c217cbb716f8e082b5ec
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-09-05 14:55:48 +0400

    removed java version check

--- local-network/consensus_client-image-build.sh
+++ local-network/consensus_client-image-build.sh
@@ -3,11 +3,5 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "${DIR}/.." || exit
 
-java_version=$(java -version 2>&1 | awk -F[\"_] '/version/ {print $2}')
-if [[ $java_version != 11* ]]; then
-  echo "Error: required Java 11, got: $java_version"
-  exit 1
-fi
-
 sbt -J-Xmx4G -J-Xss4m -Dfile.encoding=UTF-8 -Dsbt.supershell=false buildTarballsForDocker
 docker build -t unitsnetwork/consensus-client:main docker
