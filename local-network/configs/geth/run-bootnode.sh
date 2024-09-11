84ea22ee046baf46a6d4801d36b06781a0a221c4
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-09-11 13:22:57 +0400

    local-network: bootnode right IP inside script

--- local-network/configs/geth/run-bootnode.sh
+++ local-network/configs/geth/run-bootnode.sh
@@ -2,10 +2,21 @@
 
 mkdir -p /root/logs
 
+IP_RAW=$(ip -4 addr show dev eth0 | awk '/inet / {print $2}')
+IP=$(echo "$IP_RAW" | cut -d/ -f1)
+NETWORK=$(echo "$IP_RAW" | xargs ipcalc -n | awk -F= '{print $2}')
+PREFIX=$(echo "$IP_RAW" | xargs ipcalc -p | awk -F= '{print $2}')
+
+tee /root/logs/bootnode.log <<EOF
+IP: $IP
+NETWORK: $NETWORK
+PREFIX: ${PREFIX}
+EOF
+
 bootnode \
   --nodekey /bootnode.key \
-  --nat "extip:172.18.0.2" \
+  --nat "extip:${IP}" \
   --addr ":30303" \
   --verbosity=5 \
-  --netrestrict 172.18.0.0/16 \
-  > /root/logs/bootnode.log 2>&1
+  --netrestrict "${NETWORK}/${PREFIX}" \
+  2>&1 | tee /root/logs/bootnode.log
