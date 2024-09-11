dc50363f373c71284bdcb625ccb8bde695d746ef
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-09-11 12:10:57 +0400

    local-network: bootnode to exchange peers information (WIP)

--- local-network/configs/geth/run.sh
+++ local-network/configs/geth/run.sh
@@ -20,8 +20,9 @@ geth \
   --authrpc.vhosts=* \
   --authrpc.jwtsecret=/etc/secrets/jwtsecret \
   --nodekey=/etc/secrets/p2p-key \
-  --nat=none \
-  --bootnodes="${BESU_BOOTNODES}" \
+  --nat="extip:172.18.0.4" \
+  --netrestrict="172.18.0.0/16" \
+  --bootnodes="enode://7804485fc3eed8f7f5ccd61f44d6925bc3c4adb3dd9bcd417da7f74ff4e518c0367697ca74b375d134d5a73bd731076d8498391b20bd862f6b763d4299f487e2@172.18.0.2:0?discport=30303" \
   --syncmode full \
   --log.file="/root/logs/geth.log" \
   --verbosity=5 \
