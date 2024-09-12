30b47d2242c025d3c09bafee93df080ef4baa375
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-09-12 08:22:32 +0400

    Update dependencies

--- local-network/configs/besu/run-besu.sh
+++ local-network/configs/besu/run-besu.sh
@@ -2,7 +2,7 @@
 
 IP=$(hostname -I)
 
-# p2p-host = "ec-1" # Doesn't work: https://github.com/hyperledger/besu/issues/4380
+# --p2p-host="ec-1" # Doesn't work: https://github.com/hyperledger/besu/issues/4380
 besu \
   --config-file=/config/besu.conf \
   --p2p-host=${IP}
