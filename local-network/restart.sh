00097260dc83b9391c2fe15bd2e3ca21e869dc0b
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-09-25 18:28:14 +0400

    Remove the setup directory

--- local-network/restart.sh
+++ local-network/restart.sh
@@ -14,6 +14,7 @@ fi
 # We have to do this:
 # 1. docker volume can't work with symlinks.
 # 2. Just copying files makes easier code, that works both in host and docker.
+mkdir -p ./deploy/setup/{el,waves}
 cp ../src/test/resources/bridge.sol ./deploy/setup/el/
 cp ../src/test/resources/main.ride ./deploy/setup/waves/
 
