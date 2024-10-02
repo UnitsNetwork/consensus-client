38bb073f1409bb6d7efd915e66338c5d9087798d
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-10-02 19:38:31 +0400

    Fix

--- local-network/deploy/run.sh
+++ local-network/deploy/run.sh
@@ -4,5 +4,5 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "${DIR}" || exit
 
 ./bridge-compile.sh
-pip install --no-cache-dir --editable . # uncomment if you want to quickly update dependency
+# pip install --no-cache-dir --editable . # uncomment if you want to quickly update dependency
 python -B $MAIN
