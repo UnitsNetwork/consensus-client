481f080678dc068b29c097e44d74e288f3704155
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-10-09 12:21:20 +0400

    Python in local network deployment and tests (#20)

---
+++ local-network/deploy/local/common.py
@@ -0,0 +1,66 @@
+import os
+from dataclasses import dataclass
+from decimal import Decimal
+from functools import cached_property
+
+from eth_account.signers.local import LocalAccount
+from pywaves import pw
+from units_network import units
+from web3.types import Wei
+
+
+@dataclass()
+class BaseTransfer:
+    el_account: LocalAccount
+    cl_account: pw.Address
+    raw_amount: Decimal
+
+    @cached_property
+    def wei_amount(self) -> Wei:
+        return units.raw_to_wei(self.raw_amount)
+
+    @cached_property
+    def waves_atomic_amount(self) -> int:
+        return units.raw_to_waves_atomic(self.raw_amount)
+
+
+@dataclass()
+class C2ETransfer(BaseTransfer):
+    @property
+    def from_account(self) -> pw.Address:
+        return self.cl_account
+
+    @property
+    def to_account(self) -> LocalAccount:
+        return self.el_account
+
+    def __repr__(self) -> str:
+        return f"C2E(from={self.cl_account.address}, to={self.el_account.address}, {self.raw_amount} UNIT0)"
+
+
+@dataclass()
+class E2CTransfer(BaseTransfer):
+    @property
+    def from_account(self) -> LocalAccount:
+        return self.el_account
+
+    @property
+    def to_account(self) -> pw.Address:
+        return self.cl_account
+
+    def __repr__(self) -> str:
+        return f"E2C(from={self.el_account.address}, to={self.cl_account.address}, {self.raw_amount} UNIT0)"
+
+
+_INSIDE_DOCKER = None
+
+
+def in_docker() -> bool:
+    global _INSIDE_DOCKER
+    if _INSIDE_DOCKER is None:
+        try:
+            os.stat("/.dockerenv")
+            _INSIDE_DOCKER = True
+        except FileNotFoundError:
+            _INSIDE_DOCKER = False
+    return _INSIDE_DOCKER
---
+++ local-network/deploy/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "${DIR}" || exit
+
+./bridge-compile.sh
+# pip install --no-cache-dir --editable . # uncomment if you want to quickly update dependency
+python -B $MAIN
