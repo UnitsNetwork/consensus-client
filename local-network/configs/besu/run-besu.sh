67b607a08c9261bb0a92336933baaf59a38a9ee6
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2024-12-13 14:08:25 +0400

    Simpler EC setup in docker-compose.yml

--- local-network/configs/besu/run-besu.sh
+++ local-network/configs/besu/run-besu.sh
@@ -6,7 +6,9 @@ tee /opt/besu/logs/besu.log <<EOF
 IP: $IP
 EOF
 
+# https://besu.hyperledger.org/public-networks/reference/cli/options
 # --p2p-host="ec-1" # Doesn't work: https://github.com/hyperledger/besu/issues/4380
 besu \
   --config-file=/config/besu.conf \
-  --p2p-host=${IP}
+  --p2p-host=${IP} \
+  --node-private-key-file="/etc/secrets/p2p-key-${NODE_NUMBER}.hex"
