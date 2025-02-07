d70871c66a6882790f84fd2c67f139645bdd9973
Vladimir Logachev <vladimir@logachev.dev>
2025-02-07 12:50:03 +0400

    Add privateKeys to ClientConfig

--- src/main/scala/units/ClientConfig.scala
+++ src/main/scala/units/ClientConfig.scala
@@ -1,10 +1,11 @@
 package units
 
-import com.wavesplatform.account.Address
+import com.wavesplatform.account.{Address, PrivateKey}
 import com.wavesplatform.common.utils.EitherExt2
 import com.wavesplatform.common.utils.EitherExt2.explicitGet
 import com.wavesplatform.settings.*
 import pureconfig.ConfigReader
+import pureconfig.generic.semiauto.deriveReader
 import units.client.JsonRpcClient
 
 import scala.concurrent.duration.FiniteDuration
@@ -18,8 +19,9 @@ case class ClientConfig(
     blockSyncRequestTimeout: FiniteDuration,
     network: NetworkSettings,
     miningEnable: Boolean,
-    jwtSecretFile: Option[String]
-  ) derives ConfigReader {
+    jwtSecretFile: Option[String],
+    privateKeys: Seq[PrivateKey] = Seq.empty
+  ) {
   lazy val chainContractAddress: Address = Address.fromString(chainContract).explicitGet()
 
   val jsonRpcClient = JsonRpcClient.Config(
@@ -27,4 +29,8 @@ case class ClientConfig(
     apiRequestRetries = apiRequestRetries,
     apiRequestRetryWaitTime = apiRequestRetryWaitTime
   )
+}
+
+object ClientConfig {
+  given ConfigReader[ClientConfig] = deriveReader
 }
