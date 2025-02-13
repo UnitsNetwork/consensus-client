6ff60ac52397bd7829506666d9de54f044194bee
Vladimir Logachev <vladimir@logachev.dev>
2025-02-13 16:00:08 +0400

    Fix unit test

--- src/test/scala/units/MultiplePrivateKeysTestSuite.scala
+++ src/test/scala/units/MultiplePrivateKeysTestSuite.scala
@@ -5,24 +5,75 @@ import com.wavesplatform.transaction.TxHelpers
 import com.wavesplatform.wallet.Wallet
 
 class MultiplePrivateKeysTestSuite extends BaseIntegrationTestSuite {
-  private val miner = ElMinerSettings(Wallet.generateNewAccount(super.defaultSettings.walletSeed, 0))
+  private val minerFromWallet = ElMinerSettings(Wallet.generateNewAccount(super.defaultSettings.walletSeed, 0))
 
   private val miner1 = ElMinerSettings(TxHelpers.signer(1))
   private val miner2 = ElMinerSettings(TxHelpers.signer(2))
 
-  val settings: TestSettings = super.defaultSettings
-    .copy(initialMiners = List(miner, miner1, miner2))
+  override protected val defaultSettings: TestSettings = super.defaultSettings
+    // Note: For this test, the chain contract knows all the miners
+    .copy(initialMiners = List(minerFromWallet, miner1, miner2))
     .withEnabledElMining
-//    .withPrivateKeys(Seq(TxHelpers.signer(1).privateKey, TxHelpers.signer(2).privateKey)) // TODO:
 
-  "Mining starts" in {
-    val miningAttempts: Int = 1
+  "Wallet is not used when private keys are provided" in {
+    val settings = defaultSettings.withPrivateKeys(Seq(TxHelpers.signer(1).privateKey, TxHelpers.signer(2).privateKey))
     withExtensionDomain(settings) { d =>
       step("Start new epoch for ecBlock")
-      d.advanceNewBlocks(miner.address)
+      d.advanceNewBlocks(minerFromWallet.address)
       d.advanceConsensusLayerChanged()
 
-      d.ecClients.miningAttempts shouldBe miningAttempts
+      d.ecClients.miningAttempts shouldBe 0
+    }
+  }
+
+  "Wallet is used when private keys list is empty" in {
+    val settings = defaultSettings.withPrivateKeys(Seq.empty)
+
+    withExtensionDomain(settings) { d =>
+      step("Start new epoch for ecBlock")
+      d.advanceNewBlocks(minerFromWallet.address)
+      d.advanceConsensusLayerChanged()
+
+      d.ecClients.miningAttempts shouldBe 1
+    }
+  }
+
+  "Wallet is used when private keys list is not provided" in {
+    val settings = defaultSettings
+
+    withExtensionDomain(settings) { d =>
+      step("Start new epoch for ecBlock")
+      d.advanceNewBlocks(minerFromWallet.address)
+      d.advanceConsensusLayerChanged()
+
+      d.ecClients.miningAttempts shouldBe 1
+    }
+  }
+
+  "Mining starts when 1 private key is provided instead of a wallet" in {
+    val settings = defaultSettings.withPrivateKeys(Seq(TxHelpers.signer(1).privateKey))
+
+    withExtensionDomain(settings) { d =>
+      step("Start new epoch for ecBlock")
+      d.advanceNewBlocks(miner1.address)
+      d.advanceConsensusLayerChanged()
+
+      d.ecClients.miningAttempts shouldBe 1
+    }
+  }
+
+  "Mining starts when 2 private keys are provided instead of a wallet" in {
+    val settings = defaultSettings.withPrivateKeys(Seq(TxHelpers.signer(1).privateKey, TxHelpers.signer(2).privateKey))
+
+    withExtensionDomain(settings) { d =>
+      step("Start new epoch for ecBlock")
+      d.advanceNewBlocks(miner1.address)
+      d.advanceConsensusLayerChanged()
+
+      d.advanceNewBlocks(miner2.address)
+      d.advanceConsensusLayerChanged()
+
+      d.ecClients.miningAttempts shouldBe 2
     }
   }
 }
