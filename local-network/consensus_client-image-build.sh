dfd47e6895aa47976839af4262ed06aa6d88c4cf
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2025-03-18 13:03:47 +0300

    fix: We don't need to build contracts in local-network, they're built in the deploy script

--- local-network/consensus_client-image-build.sh
+++ local-network/consensus_client-image-build.sh
@@ -3,5 +3,4 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "${DIR}/.." || exit
 
-# consensus-client-it/Test/compile to compile Solidity contracts
-sbt -J-Xmx4G -J-Xss4m -Dfile.encoding=UTF-8 -Dsbt.supershell=false "docker;consensus-client-it/Test/compile"
+sbt -J-Xmx4G -J-Xss4m -Dfile.encoding=UTF-8 -Dsbt.supershell=false "docker"
