e5bcf94614eea1c65635950a3706f48da3ade824
Vladimir Logachev <vladimir@logachev.dev>
2025-03-28 18:11:38 +0400

    Add final assertion

--- consensus-client-it/src/test/scala/units/EmptyEpochMinerEvictionTestSuite.scala
+++ consensus-client-it/src/test/scala/units/EmptyEpochMinerEvictionTestSuite.scala
@@ -3,6 +3,10 @@ package units
 import com.wavesplatform.account.Address
 import com.wavesplatform.state.{Height, IntegerDataEntry, StringDataEntry}
 import com.wavesplatform.transaction.TxHelpers
+import units.el.ElBridgeClient
+import org.web3j.protocol.core.methods.response.TransactionReceipt
+import org.web3j.utils.Convert
+import scala.jdk.OptionConverters._
 
 class EmptyEpochMinerEvictionTestSuite extends BaseDockerTestSuite {
   private val reporter               = miner11Account
@@ -43,5 +47,19 @@ class EmptyEpochMinerEvictionTestSuite extends BaseDockerTestSuite {
 
     // Assertion: idle miner has been evicted
     waves1.api.dataByKey(chainContractAddress, "allMiners") shouldBe Some(StringDataEntry("allMiners", s"${reporter.toAddress}"))
+
+    // Send an L2 transaction
+    val tenGwei        = BigInt(Convert.toWei("10", Convert.Unit.GWEI).toBigIntegerExact)
+    val transferResult = elBridge.sendSendNative(elRichAccount1, clRichAccount1.toAddress, tenGwei)
+
+    // Assertion: L2 transaction is successful
+    eventually {
+      val r = ec1.web3j.ethGetTransactionReceipt(transferResult.getTransactionHash).send().getTransactionReceipt.toScala.value
+      if (!r.isStatusOK) fail(s"Expected successful sendNative, got: ${ElBridgeClient.decodeRevertReason(r.getRevertReason)}")
+      r
+    }
+
+    // Assertion: reporter started mining exactly on lastReportedHeight
+    waves1.api.height() shouldBe lastReportedHeight
   }
 }
