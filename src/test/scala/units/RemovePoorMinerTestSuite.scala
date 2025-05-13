bf536fe4c71840f9e3b90d03f3d7e6203e911dcf
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2025-05-13 18:42:36 +0400

    join: main checks go first

--- src/test/scala/units/RemovePoorMinerTestSuite.scala
+++ src/test/scala/units/RemovePoorMinerTestSuite.scala
@@ -15,19 +15,36 @@ class RemovePoorMinerTestSuite extends BaseTestSuite {
   override protected val defaultSettings: TestSettings = super.defaultSettings
     .copy(
       initialMiners = List(thisMiner, poorMiner),
-      additionalBalances = List(AddrWithBalance(newMiner.address))
+      additionalBalances = List(AddrWithBalance(newMiner.address, newMiner.wavesBalance))
     )
     .withEnabledElMining
+
+  "A new miner exists in the list" in {
+    val initialMiners = newMiner :: (1 until MaxMiners).map(i => ElMinerSettings(TxHelpers.signer(i))).toList
+    val settings      = defaultSettings.copy(initialMiners = initialMiners)
+    withExtensionDomain(settings) { d =>
+      withClue("List of miners before: ") {
+        val miners = d.chainContractClient.getAllActualMiners
+        miners.size shouldBe MaxMiners
+        miners should contain(newMiner.address)
+      }
+
+      step("A poor miner transfers all funds and a new miner joins")
+      val joinTxn = d.ChainContract.join(newMiner.account, newMiner.elRewardAddress)
+      d.appendMicroBlockE(joinTxn).left.value.getMessage should include("Already joined")
+    }
+  }
 
   "Remove poor miner test" - {
     "< 50 active miners before cleanup" in withExtensionDomain() { d =>
       withClue("List of miners before: ") {
         val miners = d.chainContractClient.getAllActualMiners
+        miners.size shouldBe <=(MaxMiners)
         miners should contain(poorMiner.address)
         miners shouldNot contain(newMiner.address)
       }
 
-      step("Poor miner transfers all funds")
+      step("A poor miner transfers all funds and a new miner joins")
       val fee            = TestValues.fee
       val transferAmount = d.balance(poorMiner.address) - fee
       val transferTxn    = TxHelpers.transfer(poorMiner.account, newMiner.address, transferAmount, fee = fee)
@@ -36,32 +53,55 @@ class RemovePoorMinerTestSuite extends BaseTestSuite {
 
       withClue("List of miners after: ") {
         val miners = d.chainContractClient.getAllActualMiners
+        miners.size shouldBe <=(MaxMiners)
         miners shouldNot contain(poorMiner.address)
         miners should contain(newMiner.address)
       }
     }
 
-    "50 active miners before cleanup" in {
-      val initialMiners = thisMiner :: poorMiner :: (1 to MaxMiners - 2).map(i => ElMinerSettings(TxHelpers.signer(i))).toList
-      val settings      = defaultSettings.copy(initialMiners = initialMiners)
-      withExtensionDomain(settings) { d =>
-        withClue("List of miners before: ") {
-          val miners = d.chainContractClient.getAllActualMiners
-          miners should contain(poorMiner.address)
-          miners shouldNot contain(newMiner.address)
+    "50 active miners before cleanup and" - {
+      "a new miner don't exist in the list with a poor miner" in {
+        val initialMiners = thisMiner :: poorMiner :: (1 to MaxMiners - 2).map(i => ElMinerSettings(TxHelpers.signer(i))).toList
+        val settings      = defaultSettings.copy(initialMiners = initialMiners)
+        withExtensionDomain(settings) { d =>
+          withClue("List of miners before: ") {
+            val miners = d.chainContractClient.getAllActualMiners
+            miners.size shouldBe MaxMiners
+            miners should contain(poorMiner.address)
+            miners shouldNot contain(newMiner.address)
+          }
+
+          step("A poor miner transfers all funds and a new miner joins")
+          val fee            = TestValues.fee
+          val transferAmount = d.balance(poorMiner.address) - fee
+          val transferTxn    = TxHelpers.transfer(poorMiner.account, newMiner.address, transferAmount, fee = fee)
+          val joinTxn        = d.ChainContract.join(newMiner.account, newMiner.elRewardAddress)
+          d.appendBlock(transferTxn, joinTxn)
+
+          withClue("List of miners after: ") {
+            val miners = d.chainContractClient.getAllActualMiners
+            miners.size shouldBe MaxMiners
+            miners shouldNot contain(poorMiner.address)
+            miners should contain(newMiner.address)
+          }
         }
+      }
 
-        step("Poor miner transfers all funds")
-        val fee            = TestValues.fee
-        val transferAmount = d.balance(poorMiner.address) - fee
-        val transferTxn    = TxHelpers.transfer(poorMiner.account, newMiner.address, transferAmount, fee = fee)
-        val joinTxn        = d.ChainContract.join(newMiner.account, newMiner.elRewardAddress)
-        d.appendBlock(transferTxn, joinTxn)
+      "and a new miner don't exist in the list with rich miners" in {
+        val initialMiners = (1 to MaxMiners - 2).map(i => ElMinerSettings(TxHelpers.signer(i))).toList ::: thisMiner :: poorMiner :: Nil
+        val settings      = defaultSettings.copy(initialMiners = initialMiners)
+        withExtensionDomain(settings) { d =>
+          withClue("List of miners before: ") {
+            val miners = d.chainContractClient.getAllActualMiners
+            log.debug(s"miners.size=${miners.toSet.size}, miners=${miners.mkString(", ")}")
+            miners.size shouldBe MaxMiners
+            miners should contain(poorMiner.address)
+            miners shouldNot contain(newMiner.address)
+          }
 
-        withClue("List of miners after: ") {
-          val miners = d.chainContractClient.getAllActualMiners
-          miners shouldNot contain(poorMiner.address)
-          miners should contain(newMiner.address)
+          step("A new miner tries to join")
+          val joinTxn = d.ChainContract.join(newMiner.account, newMiner.elRewardAddress)
+          d.appendBlockE(joinTxn).left.value.toString should include("Too many miners")
         }
       }
     }
