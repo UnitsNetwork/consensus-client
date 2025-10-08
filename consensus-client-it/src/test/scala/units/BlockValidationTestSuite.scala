74ca44be176387d4db617936ef48a5b95b456e69
Vladimir Logachev <vladimir@logachev.dev>
2025-10-08 12:09:39 +0400

    Add successful case to BlockValidationTestSuite

--- consensus-client-it/src/test/scala/units/BlockValidationTestSuite.scala
+++ consensus-client-it/src/test/scala/units/BlockValidationTestSuite.scala
@@ -14,11 +14,200 @@ import sttp.client3.{HttpClientSyncBackend, Identity, SttpBackend}
 import units.client.contract.HasConsensusLayerDappTxHelpers.EmptyE2CTransfersRootHashHex
 import units.docker.{Networks, WavesNodeContainer}
 import units.el.{DepositedTransaction, StandardBridge}
-import units.eth.EmptyL2Block
+import units.eth.{EmptyL2Block, EthAddress}
 import units.util.{BlockToPayloadMapper, HexBytesConverter}
 import units.{BlockHash, TestNetworkClient}
 
-class BlockValidationTestSuite extends BaseDockerTestSuite {
+class BlockValidationTestSuite0 extends BaseDockerTestSuite {
+  private val clSender = clRichAccount1
+  private val elRecipient = elRichAddress1
+
+  private val userAmount = 1
+  private val clAmount   = UnitsConvert.toUnitsInWaves(userAmount)
+  private val elAmount   = UnitsConvert.toWei(userAmount)
+
+  private implicit val httpClientBackend: SttpBackend[Identity, Any] = new LoggingBackend(HttpClientSyncBackend())
+  override lazy val waves1: WavesNodeContainer = new WavesNodeContainer(
+    network = network,
+    number = 1,
+    ip = Networks.ipForNode(3),
+    baseSeed = "devnet-1",
+    chainContractAddress = chainContractAddress,
+    ecEngineApiUrl = ec1.engineApiDockerUrl,
+    genesisConfigPath = wavesGenesisConfigPath,
+    enableMining = false
+  )
+
+  "Native token: Successful transfer" in {
+    step(s"EL miner 12 (${miner12Account.toAddress}) join")
+    waves1.api.broadcastAndWait(
+      ChainContract.join(
+        minerAccount = miner12Account,
+        elRewardAddress = miner12RewardAddress
+      )
+    )
+
+    step(s"EL miner 21 (${miner21Account.toAddress}) join")
+    waves1.api.broadcastAndWait(
+      ChainContract.join(
+        minerAccount = miner21Account,
+        elRewardAddress = miner21RewardAddress
+      )
+    )
+
+    val ethBalanceBefore = ec1.web3j.ethGetBalance(elRecipient.toString, DefaultBlockParameterName.LATEST).send().getBalance
+
+    step(s"Wait miner 11 (${miner11Account.toAddress}) epoch")
+    chainContract.waitForMinerEpoch(miner11Account)
+
+    step("Getting last EL block before")
+    val ecBlockBefore = ec1.engineApi.getLastExecutionBlock().explicitGet()
+
+    step("Building a simulated block")
+    val feeRecipient = miner11RewardAddress
+
+    val ntpTimestamp = System.currentTimeMillis()
+    val nanoTime     = System.nanoTime()
+    def correctedTime(): Long = {
+      val timestamp = ntpTimestamp
+      val offset    = (System.nanoTime() - nanoTime) / 1000000
+      timestamp + offset
+    }
+    val currentUnixTs   = correctedTime() / 1000
+    val blockDelay      = 6
+    val nextBlockUnixTs = (ecBlockBefore.timestamp + blockDelay).max(currentUnixTs)
+
+    def calculateRandao(hitSource: ByteStr, parentHash: BlockHash): String = {
+      val msg = hitSource.arr ++ HexBytesConverter.toBytes(parentHash)
+      HexBytesConverter.toHex(crypto.secureHash(msg))
+    }
+    val currentEpochHeader = waves1.api.blockHeader(waves1.api.height()).value
+    val hitSource          = ByteStr.decodeBase58(currentEpochHeader.VRF).get
+    val prevRandao         = calculateRandao(hitSource, ecBlockBefore.hash)
+
+    val withdrawals = Vector.empty
+
+    val depositedTransaction = StandardBridge.mkFinalizeBridgeETHTransaction(
+      transferIndex = 0L,
+      standardBridgeAddress = StandardBridgeAddress,
+      from = EthAddress.unsafeFrom(clSender.toAddress.bytes.drop(2).take(20)),
+      to = elRecipient,
+      amount = clAmount.longValue
+    )
+
+    val depositedTransactions: Seq[DepositedTransaction] = Vector(depositedTransaction)
+
+    val simulatedBlock: JsObject = ec1.engineApi
+      .simulate(
+        EmptyL2Block.mkSimulateCall(ecBlockBefore, feeRecipient, nextBlockUnixTs, prevRandao, withdrawals, depositedTransactions),
+        ecBlockBefore.hash
+      )
+      .explicitGet()
+      .head
+
+    val simulatedBlockHash = (simulatedBlock \ "hash").as[String]
+
+    step("Transfer on the chain contract")
+    waves1.api.broadcastAndWait(
+      ChainContract.transfer(
+        clSender,
+        elRecipient,
+        chainContract.nativeTokenId,
+        clAmount
+      )
+    )
+
+    step("Registering the simulated block on the chain contract")
+    waves1.api.broadcastAndWait(
+      TxHelpers.invoke(
+        invoker = miner11Account,
+        dApp = chainContractAddress,
+        func = Some("extendMainChain_v2"),
+        args = List(
+          Terms.CONST_STRING(simulatedBlockHash.drop(2)).explicitGet(),
+          Terms.CONST_STRING(ecBlockBefore.hash.drop(2)).explicitGet(),
+          Terms.CONST_BYTESTR(hitSource).explicitGet(),
+          Terms.CONST_STRING(EmptyE2CTransfersRootHashHex.drop(2)).explicitGet(),
+          Terms.CONST_LONG(0),
+          Terms.CONST_LONG(-1)
+        )
+      )
+    )
+
+    step("Sending the simulated block to waves1")
+    val payload = BlockToPayloadMapper.toPayloadJson(
+      simulatedBlock,
+      Json.obj(
+        "transactions" -> depositedTransactions.map(_.toHex),
+        "withdrawals"  -> Json.arr()
+      )
+    )
+    log.debug(s"Payload: $payload")
+    val newNetworkBlock = NetworkL2Block.signed(payload, miner11Account.privateKey).explicitGet()
+    TestNetworkClient.send("127.0.0.1", waves1.networkPort, chainContractAddress, newNetworkBlock)
+
+    step("Assertion: Block exists on EC1")
+    eventually {
+      ec1.engineApi
+        .getBlockByHash(BlockHash(simulatedBlockHash))
+        .explicitGet()
+        .getOrElse(fail(s"Block $simulatedBlockHash was not found on EC1"))
+    }
+
+    step("Assertion: Deposited transaction changes balances")
+    val ethBalanceAfter = ec1.web3j.ethGetBalance(elRecipient.toString, DefaultBlockParameterName.LATEST).send().getBalance
+    ethBalanceBefore.longValue shouldBe ethBalanceAfter.longValue - elAmount.longValue
+
+    step("Assertion: EL height grows")
+    val ecBlockAfter = ec1.engineApi.getLastExecutionBlock().explicitGet()
+    ecBlockAfter.height.longValue shouldBe (ecBlockBefore.height.longValue + 1)
+  }
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    deploySolidityContracts()
+
+    step("Enable token transfers")
+    val activationEpoch = waves1.api.height() + 1
+    waves1.api.broadcastAndWait(
+      ChainContract.enableTokenTransfersWithWaves(
+        StandardBridgeAddress,
+        WWavesAddress,
+        activationEpoch = activationEpoch
+      )
+    )
+
+    step("Set strict C2E transfers feature activation epoch")
+    waves1.api.broadcastAndWait(
+      TxHelpers.dataEntry(
+        chainContractAccount,
+        IntegerDataEntry("strictC2ETransfersActivationEpoch", activationEpoch)
+      )
+    )
+
+    step("Wait for features activation")
+    waves1.api.waitForHeight(activationEpoch)
+
+    step("Prepare: issue tokens on chain contract and transfer to a user")
+    waves1.api.broadcastAndWait(
+      TxHelpers.reissue(
+        asset = chainContract.nativeTokenId,
+        sender = chainContractAccount,
+        amount = clAmount
+      )
+    )
+    waves1.api.broadcastAndWait(
+      TxHelpers.transfer(
+        from = chainContractAccount,
+        to = clSender.toAddress,
+        amount = clAmount,
+        asset = chainContract.nativeTokenId
+      )
+    )
+  }
+}
+
+class BlockValidationTestSuite1 extends BaseDockerTestSuite {
   private implicit val httpClientBackend: SttpBackend[Identity, Any] = new LoggingBackend(HttpClientSyncBackend())
   override lazy val waves1: WavesNodeContainer = new WavesNodeContainer(
     network = network,
@@ -48,8 +237,8 @@ class BlockValidationTestSuite extends BaseDockerTestSuite {
       )
     )
 
-    val depositedTxRecipient = miner21RewardAddress
-    val ethBalanceBefore     = ec1.web3j.ethGetBalance(depositedTxRecipient.toString, DefaultBlockParameterName.LATEST).send().getBalance
+    val elRecipient      = elRichAddress1
+    val ethBalanceBefore = ec1.web3j.ethGetBalance(elRecipient.toString, DefaultBlockParameterName.LATEST).send().getBalance
 
     step(s"Wait miner 11 (${miner11Account.toAddress}) epoch")
     chainContract.waitForMinerEpoch(miner11Account)
@@ -86,7 +275,7 @@ class BlockValidationTestSuite extends BaseDockerTestSuite {
       transferIndex = 0L,
       standardBridgeAddress = StandardBridgeAddress,
       from = miner11RewardAddress,
-      to = depositedTxRecipient,
+      to = elRecipient,
       amount = 1
     )
 
@@ -104,21 +293,22 @@ class BlockValidationTestSuite extends BaseDockerTestSuite {
     val simulatedBlockHash = (simulatedBlock \ "hash").as[String]
 
     step("Registering the simulated block on the chain contract")
-    val txn = TxHelpers.invoke(
-      invoker = miner11Account,
-      dApp = chainContractAddress,
-      func = Some("extendMainChain_v2"),
-      args = List(
-        Terms.CONST_STRING(simulatedBlockHash.drop(2)).explicitGet(),
-        Terms.CONST_STRING(ecBlockBefore.hash.drop(2)).explicitGet(),
-        Terms.CONST_BYTESTR(hitSource).explicitGet(),
-        Terms.CONST_STRING(EmptyE2CTransfersRootHashHex.drop(2)).explicitGet(),
-        Terms.CONST_LONG(0),
-        Terms.CONST_LONG(-1)
+
+    waves1.api.broadcastAndWait(
+      TxHelpers.invoke(
+        invoker = miner11Account,
+        dApp = chainContractAddress,
+        func = Some("extendMainChain_v2"),
+        args = List(
+          Terms.CONST_STRING(simulatedBlockHash.drop(2)).explicitGet(),
+          Terms.CONST_STRING(ecBlockBefore.hash.drop(2)).explicitGet(),
+          Terms.CONST_BYTESTR(hitSource).explicitGet(),
+          Terms.CONST_STRING(EmptyE2CTransfersRootHashHex.drop(2)).explicitGet(),
+          Terms.CONST_LONG(0),
+          Terms.CONST_LONG(-1)
+        )
       )
     )
-
-    waves1.api.broadcastAndWait(txn) // Note: Required for full block validation
 
     step("Sending the simulated block to waves1")
     val payload = BlockToPayloadMapper.toPayloadJson(
@@ -140,10 +330,10 @@ class BlockValidationTestSuite extends BaseDockerTestSuite {
     }
 
     step("Assertion: Unexpected deposited transaction doesn't affect balances")
-    val ethBalanceAfter = ec1.web3j.ethGetBalance(depositedTxRecipient.toString, DefaultBlockParameterName.LATEST).send().getBalance
+    val ethBalanceAfter = ec1.web3j.ethGetBalance(elRecipient.toString, DefaultBlockParameterName.LATEST).send().getBalance
     ethBalanceBefore shouldBe ethBalanceAfter
 
-    step("Assertion: After timeout the height doesn't grow")
+    step("Assertion: While the block exists on EC1, the height doesn't grow")
     val ecBlockAfter = ec1.engineApi.getLastExecutionBlock().explicitGet()
     ecBlockAfter.height.longValue shouldBe ecBlockBefore.height.longValue
   }
