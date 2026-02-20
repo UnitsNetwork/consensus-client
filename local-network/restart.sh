ce66b3bf062b1cad18b5913d81d035729326ea85
Sergey Nazarov <phearnot@renee.ru>
2026-02-20 12:04:54 +0300

    fixed deploy and tests

--- local-network/restart.sh
+++ local-network/restart.sh
@@ -12,27 +12,8 @@ else
   ./delete.sh
 fi
 
-export COMPOSE_PROFILES="${COMPOSE_PROFILES:-}"
+COMPOSE_PROFILES="${COMPOSE_PROFILES:-}"
 echo "Compose profiles are: ${COMPOSE_PROFILES}"
 
-docker compose up -d
-
-# HACK: This command hangs forever in a new Docker version: docker compose logs deploy tests -f
-mapfile -t ids < <(docker compose ps -aq deploy tests)
-
-if ((${#ids[@]} > 0)); then
-  pids=()
-
-  # Follow logs in background
-  for id in "${ids[@]}"; do
-    docker logs -f "$id" &
-    pids+=("$!")
-  done
-
-  # Wait for containers to exit
-  docker wait "${ids[@]}" >/dev/null
-
-  # Stop log followers
-  kill "${pids[@]}" 2>/dev/null || true
-  wait "${pids[@]}" 2>/dev/null || true
-fi
+docker compose --profile nodes `[[ "${COMPOSE_PROFILES}" == *bs* ]] && echo --profile bs` up -d
+docker compose --profile nodes up deploy `[[ "${COMPOSE_PROFILES}" == *tests* ]] && echo tests`
